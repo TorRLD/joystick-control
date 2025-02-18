7ae6e8631acc7f2dc5d4af330bc0c0ff3fefde50
Heitor Lemos <heitorlemoss@gmail.com>
2025-02-18 16:44:02 -0300

    adicionando cabeçalho do projeto

--- joystick-control.c
+++ joystick-control.c
@@ -1,8 +1,28 @@
 /**
- * Código completo para o projeto BitDogLab com calibração do eixo Y:
- * - Controle de LEDs RGB via PWM com base no joystick (ADC) com deadzone, filtragem e calibração.
- * - Exibição de um quadrado móvel e borda com estilos alternados no display SSD1306.
- * - Uso de interrupções (com debouncing) para os botões do joystick e botão A.
+     O joystick fornecerá valores analógicos correspondentes aos eixos X e Y, que serão utilizados para:
+    Controlar a intensidade luminosa dos LEDs RGB, onde:
+        • O LED Azul terá seu brilho ajustado conforme o valor do eixo Y. Quando o joystick estiver solto
+        (posição central - valor 2048), o LED permanecerá apagado. À medida que o joystick for movido para
+        cima (valores menores) ou para baixo (valores maiores), o LED aumentará seu brilho gradualmente,
+        atingindo a intensidade máxima nos extremos (0 e 4095).
+        • O LED Vermelho seguirá o mesmo princípio, mas de acordo com o eixo X. Quando o joystick estiver
+        solto (posição central - valor 2048), o LED estará apagado. Movendo o joystick para a esquerda
+        (valores menores) ou para a direita (valores maiores), o LED aumentará de brilho, sendo mais intenso
+        nos extremos (0 e 4095).
+        • Os LEDs serão controlados via PWM para permitir variação suave da intensidade luminosa.
+        Exibir no display SSD1306 um quadrado de 8x8 pixels, inicialmente centralizado, que se moverá
+        proporcionalmente aos valores capturados pelo joystick.
+        Adicionalmente, o botão do joystick terá as seguintes funcionalidades:
+        • Alternar o estado do LED Verde a cada acionamento.
+        • Modificar a borda do display para indicar quando foi pressionado, alternando entre diferentes estilos
+        de borda a cada novo acionamento.
+
+    Finalmente, o botão A terá a seguinte funcionalidade:
+        • Ativar ou desativar os LED PWM a cada acionamento.
+
+    Por: Heitor Rodrigues Lemos Dias.
+    Fevereiro de 2025
+
  */
 
  #include <stdio.h>
